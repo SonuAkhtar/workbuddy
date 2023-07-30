177cf79597816c5fd9c5553ac9f386f5f9855b75
SonuAkhtar <sonua981@gmail.com>
2023-07-30 13:49:21 +0530

    URL's updated

--- workbuddy_backend/utils/createRequest.js
+++ workbuddy_backend/utils/createRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const createRequest = axios.create({
-  baseURL: "http:/localhost:8800/api/",
+  baseURL: "https://workbuddy-zmfs.onrender.com/api/",
   withCredentials: true,
 });
 
--- workbuddy_client/src/components/CheckoutForm/CheckoutForm.jsx
+++ workbuddy_client/src/components/CheckoutForm/CheckoutForm.jsx
@@ -54,7 +54,7 @@ const CheckoutForm = () => {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "http://localhost:5173/success",
+        return_url: "https://workbuddy-flame.vercel.app//success",
       },
     });
 
