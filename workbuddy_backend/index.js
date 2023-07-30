48c76139bfb5bb09bd2fafca94dbb6b4f60b1787
SonuAkhtar <sonua981@gmail.com>
2023-07-30 14:49:59 +0530

    updated baseURL

--- workbuddy_backend/index.js
+++ workbuddy_backend/index.js
@@ -29,7 +29,9 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://workbuddy-flame.vercel.app/", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
