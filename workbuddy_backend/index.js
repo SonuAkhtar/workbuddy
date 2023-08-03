027b0d9ca4dfe04cd5939abcdb53a45d4febfa37
SonuAkhtar <sonua981@gmail.com>
2023-08-03 14:37:45 +0530

    index.js updated

--- workbuddy_backend/index.js
+++ workbuddy_backend/index.js
@@ -31,12 +31,20 @@ const connect = async () => {
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
+
   app.use(
-    express.static(path.resolve(__dirname, "./workbuddy_client", "dist"))
+    express.static(
+      path.resolve(__dirname, "workbuddy_backend", "workbuddy_client/dist")
+    )
   );
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "./workbuddy_client", "dist", "index.html"),
+      path.resolve(
+        __dirname,
+        "workbuddy_backend",
+        "workbuddy_client/dist",
+        "index.html"
+      ),
       function (err) {
         if (err) {
           res.status(500).send(err);
